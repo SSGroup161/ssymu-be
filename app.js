b6814f46d78d78b0d37d46ef773e739a4cbfd392
SSGroup161 <shellasaukiag@gmail.com>
2025-03-05 14:20:22 +0700

    change(App.js): cors setting

--- app.js
+++ app.js
@@ -10,29 +10,29 @@ const path = require("path");
 const port = 2134;
 const app = express();
 
-// const allowedOrigins = [
-//     "http://localhost:5173",
-//     "https://levelupbeaute.id",
-//     "https://accounts.google.com",
-//     "https://www.levelupbeaute.id",
-// ];
-
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("Not allowed by CORS"));
-//         }
-//     },
-//     optionsSuccessStatus: 200,
-// };
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ssyourmakeup.id",
+    "https://www.ssyourmakeup.id",
+    "https://ssymu-fe.vercel.app",
+    "https://www.ssymu-fe.vercel.app",
+];
 
 const corsOptions = {
-    origin: "*",
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     optionsSuccessStatus: 200,
 };
+
+// const corsOptions = {
+//     origin: "*",
+//     optionsSuccessStatus: 200,
+// };
 
 app.use(helmet());
 app.use(cors(corsOptions));
